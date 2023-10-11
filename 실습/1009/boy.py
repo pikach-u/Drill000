8ce757dc647925fc0693d9dedf0092fb075bf32f
pikach-u <ziwon05@naver.com>
2023-10-11 23:39:24 +0900

    Idle state를 표현하기 위한 Idle class 추가, Decorator를 이용해서 Idle state에 필요한 함수 그룹핑

--- 실습/1009/boy.py
+++ 실습/1009/boy.py
@@ -1,14 +1,34 @@
 from pico2d import load_image
 
+class Idle:
+    @staticmethod   #decorator - 파이썬은 클래스를 객체 생성 외에도 여러 개의 함수들을 그룹핑하는 용도로도 활용 할 수 있다
+    def do():
+        print('Idle - Do')
+        pass
+
+    @staticmethod
+    def enter():
+        print('Idle - Entry Action')
+        pass
+
+    @staticmethod
+    def exit():
+        print('Idle - Exit Action')
+        pass
+
+
 
 class StateMachine:
     def __init__(self):
+        self.cur_state = Idle #파이썬은 클래스 이름도 하나의 단위. Idle Class는 함수를 그룹핑하는 용도로 사용되었기 때문에 이름만 적어줘도 가능
         pass
 
     def start(self):
+        self.cur_state.enter()
         pass
 
     def update(self):
+        self.cur_state.do()
         pass
 
     def draw(self):
