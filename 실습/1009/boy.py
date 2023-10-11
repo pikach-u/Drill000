9d6a84e8d5421d34e09896b93787f3a33eb6fdee
pikach-u <ziwon05@naver.com>
2023-10-11 23:29:31 +0900

    boy.py에 상태를 컨트롤 하는 StateMachine Class 추가

--- 실습/1009/boy.py
+++ 실습/1009/boy.py
@@ -1,6 +1,19 @@
 from pico2d import load_image
 
 
+class StateMachine:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
 class Boy:
     def __init__(self):
         self.x, self.y = 400, 90
@@ -8,12 +21,16 @@ class Boy:
         self.dir = 0
         self.action = 3
         self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine()
+        self.state_machine.start()
 
     def update(self):
         self.frame = (self.frame + 1) % 8
+        self.state_machine.update()
 
     def handle_event(self, event):
         pass
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+        self.state_machine.draw()
