90f97bed6f8ed62d45ad04a5b0bb03f5e62eb99f
pikach-u <ziwon05@naver.com>
2023-10-24 15:34:41 +0900

    object list에 depth를 위한 2개의 layer list 추가

--- 실습/1019/game_world.py
+++ 실습/1019/game_world.py
@@ -1,21 +1,26 @@
 # game world 관리 모듈
 
 # game world 표현
-objects = []    # 객체들의 리스트
+# 2개의 layer를 갖는 game world로 구현
+# 0번 list-> layer0, 1번 list-> layer1
+objects = [[], []]    # 객체들의 리스트
 
 # 월드에 객체를 넣는 함수
-def add_object(o):
-    objects.append(o)
+def add_object(o, depth=0): #기본 depth는 0. 가장 뒤
+    objects[depth].append(o)
 
 # 월드를 업데이트하는, 객체들을 모두 업데이트 하는 함수
 def update():
-    for o in objects:
-        o.update()
+    for layer in objects:
+        for o in layer:
+            o.update()
 
 # 월드 객체들을 모두 그리기
 def render():
-    for o in objects:
-        o.draw()
+    for layer in objects:
+        for o in layer:
+            o.draw()
 
 def remove_object(o):
-    objects.remove(o)
+    for layer in objects:
+        layer.remove(o)
